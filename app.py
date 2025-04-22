6b146c4b8b22a0ce16bc7eaf808be606e6de7066
jelmertamis <jelmertamis@gmail.com>
2025-04-22 19:08:52 +0200

    update

--- app.py
+++ app.py
@@ -311,7 +311,7 @@ def aio():
             'phys_in':  phys_in,
             'raw_out':  raw_out,
             'phys_out': phys_out,
-            'percent_out': percent_out,   # %
+            'percent_out': percent_out   # %
         })
     return render_template('aio.html',
                            readings=readings,
