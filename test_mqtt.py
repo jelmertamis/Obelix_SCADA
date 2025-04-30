79a0091a692f0d5464836df69738eb8da091196c
jelmertamis <jelmertamis@gmail.com>
2025-04-30 22:40:54 +0200

    sbr control set buttons nu independent en actueel alleen bij actueel nu echt

--- test_mqtt.py
+++
@@ -1,7 +0,0 @@
-# test_mqtt.py
-from obelix.mqtt_client import MQTTClient
-
-# Maak client aan (standaard in Dummy-mode)
-client = MQTTClient()
-# Verstuur testbericht
-client.publish('obelix/test', {'msg': 'hello from Windows'})
